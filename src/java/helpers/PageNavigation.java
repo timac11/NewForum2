0cacbf99765280368e86743896c693ed3cec2105
DmtrGoltsev <dmtr.goltsev@gmail.com>
2016-03-28 17:51:05 +0400

    доработка логина-регистрации
    теперь после успешной регистрации мы уже залогинены

---
+++ src/java/helpers/PageNavigation.java
@@ -0,0 +1,15 @@
+
+package helpers;
+
+/**
+ *
+ * @author User
+ */
+public class PageNavigation {
+    public static final String toLogin = "login.xhtml";
+    public static final String redirectToLogin = "login.xhtml?faces-redirect=true";
+    public static final String toIndex = "index.xhtml?faces-redirect=true";
+    public static final String toForum = "secured/forum.xhtml";
+    public static final String redirectToForum = "secured/forum.xhtml?faces-redirect=true";
+    public static final String toTopic = "secured/topic.xhtml";
+}
