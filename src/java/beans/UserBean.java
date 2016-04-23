53d10a4872058a3f1fe4e4f9d18d1899c618230e
timac11 <timac11@bk.ru>
2016-04-23 17:52:01 +0300

    правки в дизайне и профиле

--- src/java/beans/UserBean.java
+++ src/java/beans/UserBean.java
@@ -2,9 +2,14 @@
 import DAO.Factory;
 import java.io.Serializable;
 import java.sql.SQLException;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import logic.User;
+import logic.hash_password;
+import static logic.hash_password.md5Apache;
 
 @ManagedBean(name = "userBean")
 @SessionScoped
@@ -15,6 +20,12 @@ public class UserBean implements Serializable {
     private User UsFromDb;
     private String email;
     private String rights;
+    private String rpassword;
+    private String newpassword1;
+    private String newpassword2;
+    private boolean equa=false;
+    private boolean check=false;
+    private UIComponent mybutton;
 // public UserBean() throws SQLException {
    // UsersDAOimpl cn = new UsersDAOimpl();
    // User user;    
@@ -22,6 +33,25 @@ public class UserBean implements Serializable {
    // static final User user = cn.getUserByName(s);
    // user = UsersDAOimpl.getUserByName(s);
    // }
+    public void setCheck(){
+        this.check = true;
+    }
+    public boolean getCheck(){
+        return this.check;
+    }
+    
+    public void inspectCheck(){
+        check = true;
+    }
+    
+    public void setEqua(boolean e){
+        this.equa = e;
+    }
+    
+    public boolean getEqua(){
+        return equa;
+    }
+
     public String getRights (){
         return UsFromDb.getRights();
     }
@@ -59,5 +89,70 @@ public class UserBean implements Serializable {
    UsFromDb = Factory.getInstance().getUserDAO().getUserByName(s);
    return "/secured/profile.xhtml";
    }
+   
+    public void setUserFromDB(String s) throws SQLException{    
+   UsFromDb = Factory.getInstance().getUserDAO().getUserByName(s);
+  // return "/secured/profile.xhtml";
+   ;
+    }
+   
+   public String getRpassword(){
+       return this.rpassword;
+   }
+
+   public void setRpassword (String word){
+       this.rpassword = word;
+   }
+   
+   public void inspection(){
+   if(UsFromDb.getPassword().equals(md5Apache(this.rpassword,UsFromDb.getName(),UsFromDb.getEmail()))){
+       this.equa = true ;
+       this.check = false;
+   }
+  else {
+   this.equa = false ; 
+   FacesMessage msg = new FacesMessage("Invalid old password. Try again.");
+   FacesContext context = FacesContext.getCurrentInstance();
+   context.addMessage(mybutton.getClientId(context), msg);}
+   }
+  
+   public void inspection2() throws SQLException{
+   if(this.newpassword1.equals(this.newpassword2)){
+       equa = false ;
+       this.UsFromDb.setPassword(md5Apache(this.newpassword1,UsFromDb.getName(),UsFromDb.getEmail()));
+       Factory.getInstance().getUserDAO().updateUser(this.UsFromDb);
+   }
+   else {
+   equa = false ; 
+   FacesMessage msg = new FacesMessage("Password are not similar. Try again.");
+   FacesContext context = FacesContext.getCurrentInstance();
+   context.addMessage(mybutton.getClientId(context), msg);}
+   }
+   
+   public void setMybutton(UIComponent mybutton) {
+        this.mybutton = mybutton;
+    }
+
+    public UIComponent getMybutton() {
+        return mybutton;
+    }
+
+    public String getNewpassword1(){
+       return this.newpassword1;
+   }
+
+    public void setNewpassword1 (String word){
+       this.newpassword1 = word;
+   }
+   
+       public String getNewpassword2(){
+       return this.newpassword2;
+   }
+
+   public void setNewpassword2 (String word){
+       this.newpassword2 = word;
+   }
+
+
 
 }
